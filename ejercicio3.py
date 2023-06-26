773d742253e2842a851189797ae413c41f1c6b5f
Alejandro Carmona <carmona.alejandro@gmail.com>
2023-06-26 13:38:49 -0300

    ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,14 @@
+##ejercicio 3
+
+peso = input ("Ingrese su peso en Kilogramos: ")
+print ("Su peso es: ", peso)
+peso = float(peso)
+
+altura = input ("Ingrese su altura en Metros: ")
+print ("Su altura es: ", altura)
+
+altura = float (altura)
+
+imc = peso/(altura*altura)
+
+print ("Tu índice de masa corporal es: ", round(imc, 2)) 
