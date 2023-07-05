0669c1cab6b971c63089fe096304ed86f4849534
Alejandro Carmona <carmona.alejandro@gmail.com>
2023-07-05 10:19:13 -0300

    ejercicio 7

---
+++ main.py
@@ -0,0 +1,17 @@
+import operaciones
+
+def main():
+    ressuma = operaciones.suma(2,2)
+    print("Resultado de la suma:",ressuma)
+
+    resresta = operaciones.resta(2,2)
+    print("Resultado de la resta:",resresta)
+
+    resmulti = operaciones.multi(2,2)
+    print("Resultado de la multi:",resmulti)
+
+    resdivi = operaciones.divi(2,2)
+    print("Resultado de la division:",resdivi)
+
+if __name__ == "__main__":
+    main()
